fa98b5c9ef142cc0efb3e1c1d1c82b66688ae515
MishiRevenant <jm5372533@gmail.com>
2026-06-04 01:22:13 -0500

    mejora final

--- seed.js
+++ seed.js
@@ -31,11 +31,11 @@ async function seed() {
     const totalLibros = await Libro.count();
     if (totalLibros === 0) {
       await Libro.bulkCreate([
-        { titulo: 'Cien Años de Soledad',   autor: 'Gabriel García Márquez', categoria: 'Literatura',   cantidad: 3, imagen: 'https://images.penguinrandomhouse.com/cover/9780307474728' },
-        { titulo: 'El Principito',          autor: 'Antoine de Saint-Exupéry', categoria: 'Clásicos', cantidad: 5, imagen: 'https://images-na.ssl-images-amazon.com/images/I/71OZY035QkL.jpg' },
-        { titulo: 'Clean Code',             autor: 'Robert C. Martin',        categoria: 'Tecnología',  cantidad: 2, imagen: 'https://m.media-amazon.com/images/I/41xShlnTZTL._SX376_BO1,204,203,200_.jpg' },
-        { titulo: 'El Señor de los Anillos', autor: 'J.R.R. Tolkien',          categoria: 'Fantasía',    cantidad: 4, imagen: 'https://m.media-amazon.com/images/I/51EstVXM1UL._SX331_BO1,204,203,200_.jpg' },
-        { titulo: 'Sapiens',                autor: 'Yuval Noah Harari',       categoria: 'Historia',    cantidad: 3, imagen: 'https://m.media-amazon.com/images/I/41yu2qXhXXL._SX324_BO1,204,203,200_.jpg' }
+        { titulo: 'Cien Años de Soledad', autor: 'Gabriel García Márquez', categoria: 'Literatura', cantidad: 3, imagen: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjE3CPA_tuzhpPyTsBbLmmcHgVNmd62pMdRurl9jJDZcbrvbgdvqe3on1Lz0RO5zpLbguKh175MFA5H3vg-hicK3oxaftaYECyuwfF4-TT7J-BOueoxGhAf9DvzrFe0aIOS88nIcR6xEeRo7qzo4DMNQXIuEKlemMu3QDe1aoytIN9Zno3wNYJlJHaSCOU/s606/078-Cien%20a%C3%B1os%20de%20soledad-Gabriel%20Garc%C3%ADa%20M%C3%A1rquez.jpg' },
+        { titulo: 'El Principito', autor: 'Antoine de Saint-Exupéry', categoria: 'Clásicos', cantidad: 5, imagen: 'https://www.penguinlibros.com/pe/7401363-large_default/el-principito-edicion-original.webp' },
+        { titulo: 'Clean Code', autor: 'Robert C. Martin', categoria: 'Tecnología', cantidad: 2, imagen: 'https://images.cdn3.buscalibre.com/fit-in/360x360/10/fb/10fb170d7732b7dca25ebb81ded2572d.jpg' },
+        { titulo: 'El Señor de los Anillos', autor: 'J.R.R. Tolkien', categoria: 'Fantasía', cantidad: 4, imagen: 'https://images.cdn3.buscalibre.com/fit-in/360x360/db/09/db099c3235b90492cd39b8c59f32a7b5.jpg' },
+        { titulo: 'Sapiens', autor: 'Yuval Noah Harari', categoria: 'Historia', cantidad: 3, imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyAJxSA62_Z8k-pqxGp8x3Zp1bWoAtBjkZow&s' }
       ]);
       console.log('📚 Libros de ejemplo creados exitosamente.');
     } else {
